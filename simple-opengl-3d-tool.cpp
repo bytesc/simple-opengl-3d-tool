3451d18f59f470c6e2b44468f28f93c6277a2b64
rcy <21010078@mail.ecust.edu.cn>
2023-11-13 18:27:10 +0800

    ex1

--- simple-opengl-3d-tool.cpp
+++ simple-opengl-3d-tool.cpp
@@ -13,25 +13,6 @@ static point3D s_eye(5.0, 5.0, 5.0);
 static point3D s_at(0.0, 0.0, 0.0);
 static point3D A(2.0f, 0.0f, 0.0), B(2.0f, 1.0f, 0.0), C(0.0f, 1.0f, 0.0), D(1.0f, 1.0f, 1.0);
 
-void drawObject() {
-    glBegin(GL_TRIANGLES);
-    glVertex3f(A.x, A.y, A.z);
-    glVertex3f(B.x, B.y, B.z);
-    glVertex3f(C.x, C.y, C.z);
-
-    glVertex3f(A.x, A.y, A.z);
-    glVertex3f(B.x, B.y, B.z);
-    glVertex3f(D.x, D.y, D.z);
-
-    glVertex3f(B.x, B.y, B.z);
-    glVertex3f(C.x, C.y, C.z);
-    glVertex3f(D.x, D.y, D.z);
-
-    glVertex3f(C.x, C.y, C.z);
-    glVertex3f(A.x, A.y, A.z);
-    glVertex3f(D.x, D.y, D.z);
-    glEnd();
-}
 
 void Display(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -40,9 +21,14 @@ void Display(void) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0);
-
     setLookAt(iMode, s_eye, s_at);
-    drawObject();
+
+    objectID = getTetrahedron(A, B, C, D);
+    glCallList(objectID);
+
+    axisID = getAxis(5.0f); 
+    glCallList(axisID); 
+    
 
     glViewport(winWidth / 2, 0, winWidth / 2, winHeight);
     glMatrixMode(GL_PROJECTION);
@@ -50,9 +36,42 @@ void Display(void) {
     gluPerspective(45.0, (GLfloat)(winWidth / 2) / (GLfloat)winHeight, 1.0, 100.0);
 
     setLookAt(iMode, angle, s_eye, s_at);
-    drawObject();
+
+    objectID = getTetrahedron(A, B, C, D);
+    glCallList(objectID);
+
+    axisID = getAxis(3.0f); 
+    glCallList(axisID); 
 
     glutSwapBuffers();
+}
+
+void initial() {
+    glClearColor(1.0,1.0,1.0,1.0);
+}
+
+void ProcessMenu(int value) {
+    iMode = value;
+    setLookAt(iMode, s_eye, s_at);
+    setLookAt(iMode, angle, s_eye, s_at);
+    glutPostRedisplay();
+}
+void Key(unsigned char key, int x, int y) {
+
+}
+void SpecialKeys(int key, int x, int y) {
+    switch (key) {
+    case GLUT_KEY_UP:
+        angle=angle+10.0;
+        break;
+    case GLUT_KEY_DOWN:
+        angle=angle-10.0;
+        break;
+    default:
+        break;
+    }
+    setLookAt(iMode, angle, s_eye, s_at);
+    glutPostRedisplay();
 }
 
 void main(int argc, char** argv) {
@@ -60,14 +79,23 @@ void main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(winWidth, winHeight);
-    glutCreateWindow("多视区");
+    glutCreateWindow("3d");
 
     glEnable(GL_DEPTH_TEST);
 
+    int nGlutPolyMenu = glutCreateMenu(ProcessMenu);//创建菜单并定义菜单回调函数  
+    glutAddMenuEntry("XOZ(V)", 1);
+    glutAddMenuEntry("XOY(H)", 2);
+    glutAddMenuEntry("YOZ(W)", 3);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);//主菜单与鼠标右键关联
+    initial();
+
+
     glutDisplayFunc(Display);
+
     //glutReshapeFunc(ChangeSize);
-    //glutKeyboardFunc(Key);
-    //glutSpecialFunc(SpecialKeys);
+    glutKeyboardFunc(Key);
+    glutSpecialFunc(SpecialKeys);
 
     glutMainLoop();
 }
